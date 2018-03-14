8386e401198af8363edf34109603252420651d68
BeatriceDamico <damib380@newschool.edu>
2018-03-14 12:39:58 -0400

    done

---
+++ animation/animation2.js
@@ -0,0 +1,16 @@
+(function(document)){
+
+})(document);
+
+console.log("document.ready");
+
+
+setTimeout()=> {
+	$('hello-text').addClass('bounce animated')
+}, 1000);
+
+
+
+
+
+//define and execute here (pass parameters here)
